234a227a19a1d0be0c4ae817a6b7f9b708e11f5d
JamesGlik <sadoiani9900@gmail.com>
2024-04-02 12:52:52 +0400

    app.js
    added app.js with its code

---
+++ scripts/app.js
@@ -0,0 +1,35 @@
+//1
+
+let productId = 12345;
+let productName = "Apple";
+let productCategory = "Fruits";
+let productExpirationDate = "2024-02-28";
+let isProductAvailable = true;
+let productDescription;
+let productDiscount = null;
+
+console.log("Product ID:", productId);
+console.log("Product Name:", productName);
+console.log("Product Category:", productCategory);
+console.log("Expiration Date:", productExpirationDate);
+console.log("Is Product Available:", isProductAvailable);
+console.log("Product Description:", productDescription);
+console.log("Product Discount:", productDiscount);
+
+
+//2
+
+
+let myName = "Luka";
+let myHobby = "fishing";
+let yearOfBirth = "2006";
+let currentYear = "2024";
+
+let age = currentYear - yearOfBirth;
+
+let sentence1 = "My name is " + myName + ", I am " + age + " years old and my hobby is " + myHobby + ".";
+
+let sentence2 = `My name is ${myName}, I am ${age} years old and my hobby is ${myHobby}.`
+
+console.log(sentence1);
+console.log(sentence2);
